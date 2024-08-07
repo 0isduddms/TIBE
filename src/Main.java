094aa31227e3aee2ee3bbcd6e6326028605e6c98
0isduddms <dufmaqlw1@naver.com>
2024-08-07 15:36:17 +0900

    List문 작성

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+        List list = new ArrayList();
 
-        System.out.println("Hello world!");
+        list.add(1);
+        list.add(2);
+        list.add(3);
+
+        System.out.println(list.get(1));
+
     }
 }
